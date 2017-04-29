68e6486c2c843da4fb97b9f0797af130a9b7a916
raichand <shivshive04@yahoo.com>
2017-04-29 23:56:32 +0530

    code commit sele_jen_git

---
+++ src/test/java/sele_jen_git/NewTest.java
@@ -0,0 +1,15 @@
+package sele_jen_git;
+
+import org.testng.annotations.Test;
+
+
+public class NewTest {
+	
+  @Test
+  public void f() {
+	  
+	 System.out.print("Test has been completed..!");
+	  
+  }
+
+}
